028b396ecb8900ed199b378255f5e8bfe908ac77
JPHazael <jeffdaines@gmail.com>
2017-02-23 17:58:58 -0500

    Passing JSON Data to Article object

--- NewsReader/Networking Client.swift
+++ NewsReader/Networking Client.swift
@@ -14,21 +14,47 @@ struct NetworkingClient{
     static let sharedInstance = NetworkingClient()
     
     //var data:[[String: AnyObject]] = [[String: AnyObject]]()
+    //var articlesArray:[Article]? = []
     
     let key = "0201d022142c4483b285a711a61fd490"
     
     let espnURL:URL = URL(string: "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=0201d022142c4483b285a711a61fd490")!
     
     
-    func fetchArticles(){
+    func fetchArticles(url: URL, completion: @escaping([Article]) -> Void){
         
         var data:[String: AnyObject] = [String: AnyObject]()
         
-        Alamofire.request(espnURL).responseJSON { (response) in
+        Alamofire.request(url).responseJSON { (response) in
             switch response.result{
             case .success:
                 data = response.result.value as! [String: AnyObject]
-                print(data)
+                
+                
+                
+                if let articlesFromData = data["articles"] as? [[String: AnyObject]]{
+                   // print(articlesFromData)
+                    var articlesArray:[Article]? = []
+
+                    for articleFromData in articlesFromData {
+                        let article = Article()
+                        
+                    if let headline = articleFromData["title"] as? String, let author = articleFromData["author"] as? String, let desc = articleFromData["description"] as? String, let articleURL = articleFromData["URL"] as? String, let imageURL = articleFromData["urltoimage"] as? String{
+                        
+                        
+                            article.author = author
+                            article.headline = headline
+                            article.desc = desc
+                            article.URL = articleURL
+                            article.imageURL = imageURL
+                        
+                        }
+                    articlesArray?.append(article)
+                   // print(articlesArray)
+                    completion(articlesArray!)
+                    }
+                    
+                }
             case .failure(let error):
                 print(error)
 
--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -9,19 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var espnData = [String: AnyObject]()
+    var articlesArray:[Article]? = []
 
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        NetworkingClient.sharedInstance.fetchArticles()
+        NetworkingClient.sharedInstance.fetchArticles(url: NetworkingClient.sharedInstance.espnURL, completion:{ (data) in
+        
+            self.articlesArray = data
+            //print(self.articlesArray)
+            self.tableView.reloadData()
+        })
     }
    
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return (self.articlesArray?.count)!
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -30,6 +40,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell") as! ArticleTableViewCell!
+        
+        cell?.authorLabel.text = articlesArray?[indexPath.row].author
         
         return cell!
     }
