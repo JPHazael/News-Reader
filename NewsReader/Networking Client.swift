78d2d931dff96b611caa139143fcb83be65fda03
JPHazael <jeffdaines@gmail.com>
2017-02-23 14:04:18 -0500

    Started working on networking client.

--- NewsReader/Networking Client.swift
+++ NewsReader/Networking Client.swift
@@ -7,8 +7,36 @@
 //
 
 import Foundation
+import Alamofire
 
 struct NetworkingClient{
+    
+    static let sharedInstance = NetworkingClient()
+    
+    //var data:[[String: AnyObject]] = [[String: AnyObject]]()
+    
+    let key = "0201d022142c4483b285a711a61fd490"
+    
+    let espnURL:URL = URL(string: "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=0201d022142c4483b285a711a61fd490")!
+    
+    
+    func fetchArticles(){
+        
+        var data:[String: AnyObject] = [String: AnyObject]()
+        
+        Alamofire.request(espnURL).responseJSON { (response) in
+            switch response.result{
+            case .success:
+                data = response.result.value as! [String: AnyObject]
+                print(data)
+            case .failure(let error):
+                print(error)
+
+            
+            }
+        }
+        
+    }
     
     
     
--- NewsReader/ViewController.swift
+++ NewsReader/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        NetworkingClient.sharedInstance.fetchArticles()
     }
    
     
